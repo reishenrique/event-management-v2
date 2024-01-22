09cf9b7080d2dc9aeeab1ed71d2455459a5bc1d5
Henrique Reis <contatohenriquereis@gmail.com>
2024-01-22 14:29:33 -0300

    feature: server configuration file

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,17 @@
-import fastify from 'fastify'
-import 'dotenv/config'
+import mongoose from 'mongoose'
+import express from 'express'
+import { env } from '../src/database/env'
 
-export const app = fastify()
+const app = express()
+const port = env.PORT || 3000
 
-app.listen({ host: '0.0.0.0', port: 3000 }).then(() => {
-  console.log(`Server Runnnig on port 3000`)
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+mongoose.connect(env.DATABASE_CONNECTION as string).then(() => {
+  app
+    .listen(port, () => {
+      console.log(`MongoDB Connected / Server running on port: ${port}`)
+    })
+    .catch((error) => console.log(`Fail to connect to the server: ${error}`))
 })
