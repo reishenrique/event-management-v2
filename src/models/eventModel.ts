6968da7394e3fff9790a1d2f7aba8a8e4deb43ee
Henrique Reis <contatohenriquereis@gmail.com>
2024-01-23 19:25:29 -0300

    feature: database modeling for events

---
+++ src/models/eventModel.ts
@@ -0,0 +1,31 @@
+import { Schema, model } from 'mongoose'
+
+interface IEventModel {
+  eventName: string
+  descriptionEvent: string
+  date: Date
+  location: string
+  typeEvent: string
+  ticketPrice: number
+  venueCapacity: number
+  contactInformation: string
+  paymentMethods: string
+  createdAt: Date
+}
+
+const eventModelSchema = new Schema<IEventModel>({
+  eventName: { type: String, required: true },
+  descriptionEvent: { type: String, required: true },
+  date: { type: Date, required: true },
+  location: { type: String, required: true },
+  typeEvent: { type: String, required: true },
+  ticketPrice: { type: Number, required: true },
+  venueCapacity: { type: Number, required: true },
+  contactInformation: { type: String, required: true },
+  paymentMethods: { type: String, required: true },
+  createdAt: { type: Date, require: true },
+})
+
+const EventModel = model<IEventModel>('Event', eventModelSchema)
+
+export { EventModel }
