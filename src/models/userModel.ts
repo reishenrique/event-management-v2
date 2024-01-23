5ce73480a5d4cc27bdb97a50a481aa3f99368fcd
Henrique Reis <contatohenriquereis@gmail.com>
2024-01-23 19:25:54 -0300

    feature: database modeling for users

---
+++ src/models/userModel.ts
@@ -0,0 +1,29 @@
+import { Schema, model } from 'mongoose'
+
+interface IUserModel {
+  firstName: string
+  lastName: string
+  cpf: string
+  emailAddress: string
+  userName: string
+  phoneNumber: string
+  dateOfBirth: Date
+  gender: string
+  createdAt: Date
+}
+
+const userModelSchema = new Schema<IUserModel>({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  cpf: { type: String, required: true },
+  emailAddress: { type: String, required: true },
+  userName: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  dateOfBirth: { type: Date, required: true },
+  gender: { type: String, required: false },
+  createdAt: { type: Date, require: true },
+})
+
+const UserModel = model<IUserModel>('User', userModelSchema)
+
+export { UserModel }
