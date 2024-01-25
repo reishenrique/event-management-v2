437a78b8a113a86108d80b930db868b194a3aba5
Henrique Reis <contatohenriquereis@gmail.com>
2024-01-25 14:54:26 -0300

    feature: setting up the use of routes on the server

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import mongoose from 'mongoose'
 import express from 'express'
 import { env } from '../src/database/env'
+import routes from './routes'
 
 const app = express()
 const port = env.PORT || 3000
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+app.use(routes)
 
 mongoose
   .connect(env.DATABASE_CONNECTION as string)
