053b6c20b6f41e1cba0da4af247dd4e8807e4709
Henrique Reis <contatohenriquereis@gmail.com>
2024-01-29 19:42:28 -0300

    feature: data structure and validation for event registration

---
+++ src/controllers/eventController.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from 'express'
+import { z } from 'zod'
+
+export class EventController {
+  async createEvent(req: Request, res: Response) {
+    const eventSchema = z.object({
+      eventName: z
+        .string({ required_error: 'Event name is required' })
+        .optional(),
+      eventDescription: z
+        .string({ required_error: 'Description is required' })
+        .optional(),
+      date: z
+        .date({ required_error: 'Event date and time are required' })
+        .optional(),
+      eventLocation: z
+        .string({ required_error: 'Event location is required' })
+        .optional(),
+      eventType: z
+        .string({ required_error: 'Event type is required' })
+        .optional(),
+      eventTicketPrice: z
+        .string({
+          required_error: 'The event ticket price is required',
+        })
+        .optional(),
+      venueCapacity: z
+        .number({
+          required_error: 'The venue capacity for the event is required',
+        })
+        .optional(),
+      contactInformation: z.string().optional(),
+    })
+
+    const event = eventSchema.parse(req.body)
+  }
+}
