1c8d099864d5017187697932ca50f71452a6ca2d
Henrique Reis <contatohenriquereis@gmail.com>
2024-02-05 11:20:52 -0300

    feature: add create event route

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
-import { Router } from "express";
-import UserController from "../src/controllers/userController";
+import { Router } from 'express'
+import UserController from '../src/controllers/userController'
+import EventController from '../src/controllers/eventController'
 
-const routes = Router();
+const routes = Router()
 
-routes.post("/createUser", new UserController().createUser);
+routes.post('/createUser', new UserController().createUser)
 
-export default routes;
+routes.post('/createEvent', new EventController().createEvent)
+
+export default routes
