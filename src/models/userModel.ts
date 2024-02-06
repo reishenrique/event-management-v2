5c34bfabb6a3996984a587aed7aeca4f954b6e2c
Henrique Reis <contatohenriquereis@gmail.com>
2024-02-06 12:18:00 -0300

    refactor: adding the cnpj property to the user model

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,27 +1,29 @@
-import { Schema, model } from "mongoose";
+import { Schema, model } from 'mongoose'
 
 interface IUserModel {
-  firstName: string;
-  lastName: string;
-  userName: string;
-  cpf: string;
-  emailAddress: string;
-  phoneNumber: string;
-  dateOfBirth: Date;
-  gender: string;
+  firstName: string
+  lastName: string
+  userName: string
+  cpf: string
+  cnpj: string
+  emailAddress: string
+  phoneNumber: string
+  dateOfBirth: Date
+  gender: string
 }
 
 const userModelSchema = new Schema<IUserModel>({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   cpf: { type: String, required: true },
+  cnpj: { type: String, required: true },
   emailAddress: { type: String, required: true },
   userName: { type: String, required: true },
   phoneNumber: { type: String, required: true },
   dateOfBirth: { type: Date, required: true },
   gender: { type: String, required: false },
-});
+})
 
-const UserModel = model<IUserModel>("User", userModelSchema);
+const UserModel = model<IUserModel>('User', userModelSchema)
 
-export { UserModel, IUserModel };
+export { UserModel, IUserModel }
