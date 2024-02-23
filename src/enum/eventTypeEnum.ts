55d0620e28777018a2fe90d15362e16bffac38cc
Henrique Reis <contatohenriquereis@gmail.com>
2024-02-23 13:00:25 -0300

    feature: enum for event types and payment types

---
+++ src/enum/eventTypeEnum.ts
@@ -0,0 +1,8 @@
+export enum eventTypeEnum {
+  SOCIAL_EVENT = 'Social Event',
+  CORPORATE_EVENT = 'Corporate Event',
+  RELIGIOUS_EVENT = 'Religious Event',
+  ACADEMIC_EDUCATIONAL_EVENT = 'Academic and Educational Event',
+  CULTURAL_ENTERTAINMENT_EVENT = 'Cultural Entertainment Event',
+  SPORTING_EVENT = 'Sporting Event',
+}
---
+++ src/enum/paymentMethodEnum.ts
@@ -0,0 +1,5 @@
+export enum paymentMethodEnum {
+  CREDIT = 'Credit',
+  DEBIT = 'Debit',
+  BANK_SLIP = 'Bank Slip',
+}
