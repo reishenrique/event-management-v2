9f761e5dd67589d45ebce14594ee374648c817fa
Henrique Reis <contatohenriquereis@gmail.com>
2024-02-23 12:03:59 -0300

    refactor: linting server file

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,21 @@
-import mongoose from "mongoose";
-import { app } from "../src/app";
-import "dotenv/config";
+import mongoose from 'mongoose'
+import { app } from '../src/app'
+import 'dotenv/config'
 
-const port = process.env.PORT || 3000;
-const stringConnection = process.env.DATABASE_CONNECTION as string;
+const port = process.env.PORT || 3000
+const stringConnection = process.env.DATABASE_CONNECTION as string
 
 async function connectDatabase() {
   const mongooseConnection = mongoose
     .connect(stringConnection)
     .then(() => {
       app.listen(port, () => {
-        console.log(`MongoDB Connected / Server running on port: ${port}`);
-      });
+        console.log(`MongoDB Connected / Server running on port: ${port}`)
+      })
     })
-    .catch((error: Error) => console.log(error));
+    .catch((error: Error) => console.log(error))
 
-  return mongooseConnection;
+  return mongooseConnection
 }
 
-connectDatabase();
+connectDatabase()
