af96ca0ab6df30686372604c49b22944107875bc
Henrique Reis <contatohenriquereis@gmail.com>
2024-02-26 13:49:42 -0300

    feature: adding routes for searching by event ID and CNPJ

--- src/routes.ts
+++ src/routes.ts
@@ -5,11 +5,13 @@ import EventController from '../src/controllers/eventController'
 const routes = Router()
 
 routes.post('/user/createUser', new UserController().createUser)
-routes.get('/user/bycpf/:cpf', new UserController().getUserByCpf)
-routes.get('/user/byid/:id', new UserController().getUserById)
+routes.get('/user/getbycpf/:cpf', new UserController().getUserByCpf)
+routes.get('/user/getbyid/:id', new UserController().getUserById)
 routes.delete('/user/:id', new UserController().deleteUserById)
 routes.put('/user/:id', new UserController().updateUserById)
 
 routes.post('/event/createEvent', new EventController().createEvent)
+routes.get('/event/getbyid/:id', new EventController().getEventById)
+routes.get('/event/getbycnpj/:cnpj', new EventController().getEventByCnpj)
 
 export default routes
