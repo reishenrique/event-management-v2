94a955d236119b45515fd947f847087c3cf7db3b
Henrique Reis <contatohenriquereis@gmail.com>
2024-03-01 16:14:00 -0300

    refactor: passing the verify token middleware to the route

--- src/routes/authRoute/authRoutes.ts
+++ src/routes/authRoute/authRoutes.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express'
 import AuthController from '../../controllers/authController'
-import Authorization from '../../middlewares/authorization'
+import verifyToken from '../../middlewares/authorization'
 
 const authRoutes = Router()
 
 authRoutes.post('/auth/login', new AuthController().login)
 authRoutes.post(
   '/auth/verifytoken',
-  new Authorization().verifyToken,
+  verifyToken,
   new AuthController().authenticated,
 )
 
