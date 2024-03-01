637a19a5fcdca7ace24f05bd9db72bce4da1052a
Henrique Reis <contatohenriquereis@gmail.com>
2024-03-01 16:13:37 -0300

    refactor: transforming the verify token into a function instead of a class

--- src/middlewares/authorization.ts
+++ src/middlewares/authorization.ts
@@ -3,42 +3,32 @@ import { StatusCodes } from 'http-status-codes'
 import jwt from 'jsonwebtoken'
 import 'dotenv/config'
 
-interface IAuthorization {
-  verifyToken(req: Request, res: Response, next: NextFunction): Promise<object>
-}
+const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+  const secret = process.env.SECRET
 
-class Authorization implements IAuthorization {
-  async verifyToken(
-    req: Request,
-    res: Response,
-    next: NextFunction,
-  ): Promise<any> {
-    const secret = process.env.SECRET
+  const tokenHeader = req.headers.authorization
+  // console.log('TokenHeader:', tokenHeader)
 
-    const tokenHeader = req.headers.authorization
-    console.log('TokenHeader:', tokenHeader)
+  const token = tokenHeader && tokenHeader.split(' ')[1]
+  // console.log('Token:', token)
 
-    const token = tokenHeader && tokenHeader.split(' ')[1]
-    console.log('Token:', token)
+  if (!token) {
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ message: 'Token not provided' })
+  }
 
-    if (!token) {
-      return res
-        .status(StatusCodes.UNAUTHORIZED)
-        .json({ message: 'Token not provided' })
-    }
+  try {
+    jwt.verify(token, secret || '')
 
-    try {
-      jwt.verify(token, secret || '')
+    next()
+  } catch (error) {
+    console.log('Error while executing the verify token endpoint', error)
 
-      next()
-    } catch (error) {
-      console.log('Error while executing the verify token endpoint', error)
-
-      return res
-        .status(StatusCodes.UNAUTHORIZED)
-        .json({ message: 'Invalid Token' })
-    }
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ message: 'Invalid Token' })
   }
 }
 
-export default Authorization
+export default verifyToken
