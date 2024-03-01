05373b118afea26c182857b57c36a1bbe2014e3e
Henrique Reis <contatohenriquereis@gmail.com>
2024-03-01 11:40:36 -0300

    feature: adding route using token verification middleware and authenticated route

--- src/routes/authRoute/authRoutes.ts
+++ src/routes/authRoute/authRoutes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express'
 import AuthController from '../../controllers/authController'
+import Authorization from '../../middlewares/authorization'
 
 const authRoutes = Router()
 
 authRoutes.post('/auth/login', new AuthController().login)
+authRoutes.post(
+  '/auth/verifytoken',
+  new Authorization().verifyToken,
+  new AuthController().authenticated,
+)
 
 export default authRoutes
