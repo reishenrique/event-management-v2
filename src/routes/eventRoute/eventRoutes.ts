78e48c2a1bdcf5a814c9c0675c368806a77155ac
Henrique Reis <contatohenriquereis@gmail.com>
2024-03-06 12:41:54 -0300

    feature: add authentication to event routes

--- src/routes/eventRoute/eventRoutes.ts
+++ src/routes/eventRoute/eventRoutes.ts
@@ -1,12 +1,37 @@
 import { Router } from 'express'
 import EventController from '../../controllers/eventController'
+import verifyToken from '../../middlewares/authorization'
 
 const eventRoutes = Router()
 
-eventRoutes.post('/event/createEvent', new EventController().createEvent)
-eventRoutes.get('/event/getbyid/:id', new EventController().getEventById)
-eventRoutes.get('/event/getbycnpj/:cnpj', new EventController().getEventByCnpj)
-eventRoutes.delete('/event/:id', new EventController().deleteEventById)
-eventRoutes.put('/event/:id', new EventController().updateEventById)
+eventRoutes.post(
+  '/event/createEvent',
+  verifyToken,
+  new EventController().createEvent,
+)
+
+eventRoutes.get(
+  '/event/getbyid/:id',
+  verifyToken,
+  new EventController().getEventById,
+)
+
+eventRoutes.get(
+  '/event/getbycnpj/:cnpj',
+  verifyToken,
+  new EventController().getEventByCnpj,
+)
+
+eventRoutes.delete(
+  '/event/:id',
+  verifyToken,
+  new EventController().deleteEventById,
+)
+
+eventRoutes.put(
+  '/event/:id',
+  verifyToken,
+  new EventController().updateEventById,
+)
 
 export default eventRoutes
