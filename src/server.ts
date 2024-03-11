438367163550c0bb10d761f733418c41c4e38109
Henrique Reis <contatohenriquereis@gmail.com>
2024-03-11 12:14:06 -0300

    refactor: encapsulating the database connection function and express server

--- src/server.ts
+++ src/server.ts
@@ -5,15 +5,13 @@ import 'dotenv/config'
 const port = process.env.PORT || 3000
 const stringConnection = process.env.DATABASE_CONNECTION as string
 
-async function connectDatabase() {
-  const mongooseConnection = mongoose
-    .connect(stringConnection)
-    .then(() => {
-      app.listen(port, () => {
-        console.log(`MongoDB Connected / Server running on port: ${port}`)
-      })
+const connectDatabase = async () => {
+  const mongooseConnection = await mongoose.connect(stringConnection)
+  if (mongooseConnection) {
+    app.listen(port, () => {
+      console.log(`MongoDB Connected / Server running on port: ${port}`)
     })
-    .catch((error: Error) => console.log(error))
+  }
 
   return mongooseConnection
 }
