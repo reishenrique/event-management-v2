0efa1c122ed8f933346f5e26d26d8c43266790c7
Henrique Reis <contatohenriquereis@gmail.com>
2024-03-18 12:53:51 -0300

    feature: implementing a use case getUserByCpf

---
+++ src/domain/useCases/users/getUserByCpf.useCase.ts
@@ -0,0 +1,23 @@
+import { IUserRepository } from '../../interfaces/IUserRepository'
+
+export class GetUserByCpfUseCase {
+  constructor(private userRepository: IUserRepository) {
+    this.userRepository = userRepository
+  }
+
+  async execute(cpf: string) {
+    if (!cpf) {
+      throw new Error(
+        'User ID is required to proceed with the search execution',
+      )
+    }
+
+    const getUserByCpf = await this.userRepository.findDocumentUserByCpf(cpf)
+
+    if (!getUserByCpf) {
+      throw new Error('User not found or registered')
+    }
+
+    return getUserByCpf
+  }
+}
