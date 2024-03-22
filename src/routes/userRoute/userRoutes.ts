c041cae5fb1f93a53aa4df4fb84d61cb73eef81d
Henrique Reis <contatohenriquereis@gmail.com>
2024-03-22 11:41:40 -0300

    feature: implementing dependency injection of the useCase using the user repository and passing it to the controller

--- src/routes/userRoute/userRoutes.ts
+++ src/routes/userRoute/userRoutes.ts
@@ -4,6 +4,7 @@ import UserController from '../../controllers/userController'
 import { CreateUserUseCase } from '../../domain/useCases/users/createUser.useCase'
 import { GetUserByCpfUseCase } from '../../domain/useCases/users/getUserByCpf.useCase'
 import { GetUserByIdUseCase } from '../../domain/useCases/users/getUserById.useCase'
+import UpdateUserByIdUseCase from '../../domain/useCases/users/updateUserById.useCase'
 
 const userRoutes = Router()
 
@@ -12,17 +13,19 @@ const userRepository = new UserRepository()
 const createUserUseCase = new CreateUserUseCase(userRepository)
 const getUserByCpfUseCase = new GetUserByCpfUseCase(userRepository)
 const getUserByIdUseCase = new GetUserByIdUseCase(userRepository)
+const updateUserByIdUseCase = new UpdateUserByIdUseCase(userRepository)
 
 const userController = new UserController(
   createUserUseCase,
   getUserByCpfUseCase,
   getUserByIdUseCase,
+  updateUserByIdUseCase,
 )
 
 userRoutes.post('/user/createUser', userController.createUser)
 userRoutes.get('/user/getbycpf/:cpf', userController.getUserByCpf)
 userRoutes.get('/user/getbyid/:id', userController.getUserById)
-userRoutes.delete('/user/:id', userController.deleteUserById)
 userRoutes.put('/user/:id', userController.updateUserById)
+userRoutes.delete('/user/:id', userController.deleteUserById)
 
 export default userRoutes
