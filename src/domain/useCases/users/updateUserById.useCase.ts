dbc083448d07a76959c2137cf561e44308a9c0d9
Henrique Reis <contatohenriquereis@gmail.com>
2024-03-22 11:40:51 -0300

    feature: implementing the use case for searching by ID and updating user data

---
+++ src/domain/useCases/users/updateUserById.useCase.ts
@@ -0,0 +1,24 @@
+import { IUserRepository } from '../../interfaces/IUserRepository'
+
+export default class UpdateUserByIdUseCase {
+  constructor(private userRepository: IUserRepository) {
+    this.userRepository = userRepository
+  }
+
+  async execute(id: string, dataUserUpdate: string) {
+    if (!id) {
+      throw new Error('User ID is required to proceed with update execution')
+    }
+
+    const findUserAndUpdate = await this.userRepository.findUserByIdAndUpdate(
+      id,
+      dataUserUpdate,
+    )
+
+    if (!findUserAndUpdate) {
+      throw new Error('User not found for update')
+    }
+
+    return findUserAndUpdate
+  }
+}
