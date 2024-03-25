1da66e42fe6f020682cc236108050ecb056938c7
Henrique Reis <contatohenriquereis@gmail.com>
2024-03-25 14:20:25 -0300

    feature: event entity

---
+++ src/domain/entities/EventEntity.ts
@@ -0,0 +1,11 @@
+export interface EventEntity {
+  eventName?: string
+  eventDescription?: string
+  cnpj?: string
+  location?: string
+  eventType?: string
+  eventTicketPrice?: number
+  venueCapacity?: number
+  contactInformation?: string
+  paymentMethodOption?: string
+}
