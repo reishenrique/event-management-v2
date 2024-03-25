01dc4999b92ab6d7782a9607f6ab7b77693bd1ac
Henrique Reis <contatohenriquereis@gmail.com>
2024-03-25 14:21:59 -0300

    refactor: implementing dependency injection in the event routes.

--- src/routes/eventRoute/eventRoutes.ts
+++ src/routes/eventRoute/eventRoutes.ts
@@ -1,37 +1,25 @@
 import { Router } from 'express'
 import EventController from '../../controllers/eventController'
 import verifyToken from '../../middlewares/authorization'
+import { EventRepository } from '../../application/repositories/eventRepository'
+import { CreateEventUseCase } from '../../domain/useCases/events/createEvent.useCase'
 
 const eventRoutes = Router()
 
-eventRoutes.post(
-  '/event/createEvent',
-  verifyToken,
-  new EventController().createEvent,
-)
+const eventRepository = new EventRepository()
 
-eventRoutes.get(
-  '/event/getbyid/:id',
-  verifyToken,
-  new EventController().getEventById,
-)
+const createEventUseCase = new CreateEventUseCase(eventRepository)
 
+const eventController = new EventController(createEventUseCase)
+
+eventRoutes.post('/event/createEvent', verifyToken, eventController.createEvent)
+eventRoutes.get('/event/getbyid/:id', verifyToken, eventController.getEventById)
 eventRoutes.get(
   '/event/getbycnpj/:cnpj',
   verifyToken,
-  new EventController().getEventByCnpj,
+  eventController.getEventByCnpj,
 )
-
-eventRoutes.delete(
-  '/event/:id',
-  verifyToken,
-  new EventController().deleteEventById,
-)
-
-eventRoutes.put(
-  '/event/:id',
-  verifyToken,
-  new EventController().updateEventById,
-)
+eventRoutes.delete('/event/:id', verifyToken, eventController.deleteEventById)
+eventRoutes.put('/event/:id', verifyToken, eventController.updateEventById)
 
 export default eventRoutes
