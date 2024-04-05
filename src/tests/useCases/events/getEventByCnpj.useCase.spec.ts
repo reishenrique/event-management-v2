24c002ec955726f5b275fa2cb8df67c5d11dd604
Henrique Reis <contatohenriquereis@gmail.com>
2024-04-05 11:24:24 -0300

    feature: unit test that throws an exception when the event is not found by the cnpj

--- src/tests/useCases/events/getEventByCnpj.useCase.spec.ts
+++ src/tests/useCases/events/getEventByCnpj.useCase.spec.ts
@@ -1,3 +1,4 @@
+import { CustomError } from '../../../domain/errors/customError'
 import { IEventRepository } from '../../../domain/interfaces/IEventRepository'
 import { GetEventByCnpjUseCase } from '../../../domain/useCases/events/getEventByCnpj.useCase'
 import { EventRepositoryInMemory } from '../../mock/eventRepositoryInMemory'
@@ -41,5 +42,30 @@ describe('Get event by cnpj', () => {
     expect(user).toEqual(mockEvent)
 
     expect(mockEventRepository.findEventByCnpj).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should throw an exception when the event cnpj is not provided', async () => {
+    const mockEvent = {
+      _id: '6602d505cfa1f38f545a4542',
+      eventName: 'Mock Event',
+      eventDescription: 'Mock Event',
+      cnpj: '12345678901220',
+      location: 'São Paulo',
+      eventType: 'Social Event',
+      eventTicketPrice: 1000,
+      venueCapacity: 1000,
+      contactInformation: 'Test mock event',
+      paymentMethod: 'Credit',
+    }
+
+    const { sut, mockEventRepository } = makeSut([mockEvent])
+
+    await expect(sut.execute(undefined)).rejects.toThrow(
+      CustomError.BadRequestError(
+        'Event CNPJ is required to proceed with the search execution',
+      ),
+    )
+
+    expect(mockEventRepository.findEventByCnpj).not.toHaveBeenCalled()
   })
 })
