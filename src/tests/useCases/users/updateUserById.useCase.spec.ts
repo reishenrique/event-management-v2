ff7fa3a6511c0876404bca7ff7134d9c428f0eef
Henrique Reis <contatohenriquereis@gmail.com>
2024-04-08 10:19:50 -0300

    feature: unit test that returns an exception when the user ID is not provided for search and update

--- src/tests/useCases/users/updateUserById.useCase.spec.ts
+++ src/tests/useCases/users/updateUserById.useCase.spec.ts
@@ -1,3 +1,4 @@
+import { CustomError } from '../../../domain/errors/customError'
 import { IUserRepository } from '../../../domain/interfaces/IUserRepository'
 import { UpdateUserByIdUseCase } from '../../../domain/useCases/users/updateUserById.useCase'
 import { UserRepositoryInMemory } from '../../mock/userRepositoryInMemory'
@@ -47,5 +48,34 @@ describe('Update user by id', () => {
     expect(user.lastName).toBe('Gaúcho')
 
     expect(mockUserRepository.findUserByIdAndUpdate).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return an exception when the user ID is not provided for search and update', async () => {
+    const mockUser = {
+      _id: '65ff47ddd240477491b399bf',
+      firstName: 'John',
+      lastName: 'Doe',
+      cpf: '12345678901',
+      password: 'genericpassword',
+      emailAddress: 'johndoe@example.com',
+      userName: 'john.doe',
+      phoneNumber: '+1234567890',
+      gender: 'Male',
+    }
+
+    const { sut, mockUserRepository } = makeSut([mockUser])
+
+    const newUserData = {
+      firstName: 'Ronaldinho',
+      lastName: 'Gaúcho',
+    }
+
+    await expect(sut.execute(undefined, newUserData)).rejects.toThrow(
+      CustomError.BadRequestError(
+        'User ID is required to proceed with update execution',
+      ),
+    )
+
+    expect(mockUserRepository.findUserByIdAndUpdate).not.toHaveBeenCalled()
   })
 })
