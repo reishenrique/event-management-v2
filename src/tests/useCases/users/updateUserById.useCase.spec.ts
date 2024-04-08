661c4be28d8484e246266330568c312a1f301065
Henrique Reis <contatohenriquereis@gmail.com>
2024-04-08 10:03:02 -0300

    feature: unit test that searches for a user by ID and updates their data

---
+++ src/tests/useCases/users/updateUserById.useCase.spec.ts
@@ -0,0 +1,51 @@
+import { IUserRepository } from '../../../domain/interfaces/IUserRepository'
+import { UpdateUserByIdUseCase } from '../../../domain/useCases/users/updateUserById.useCase'
+import { UserRepositoryInMemory } from '../../mock/userRepositoryInMemory'
+
+const makeSut = (
+  users?: any,
+): { sut: UpdateUserByIdUseCase; mockUserRepository: IUserRepository } => {
+  const mockUserRepository = new UserRepositoryInMemory(users)
+  const sut = new UpdateUserByIdUseCase(mockUserRepository)
+
+  jest.spyOn(mockUserRepository, 'findUserByIdAndUpdate')
+
+  return { sut, mockUserRepository }
+}
+
+describe('Update user by id', () => {
+  beforeEach(async () => {
+    jest.clearAllMocks()
+  })
+
+  it('Should search for a user by ID and update the data', async () => {
+    const mockUser = {
+      _id: '65ff47ddd240477491b399bf',
+      firstName: 'John',
+      lastName: 'Doe',
+      cpf: '12345678901',
+      password: 'genericpassword',
+      emailAddress: 'johndoe@example.com',
+      userName: 'john.doe',
+      phoneNumber: '+1234567890',
+      gender: 'Male',
+    }
+
+    const { sut, mockUserRepository } = makeSut([mockUser])
+
+    const id = '65ff47ddd240477491b399bf'
+
+    const newUserData = {
+      firstName: 'Ronaldinho',
+      lastName: 'Gaúcho',
+    }
+
+    const user = await sut.execute(id, newUserData)
+    console.log(user)
+
+    expect(user.firstName).toBe('Ronaldinho')
+    expect(user.lastName).toBe('Gaúcho')
+
+    expect(mockUserRepository.findUserByIdAndUpdate).toHaveBeenCalledTimes(1)
+  })
+})
