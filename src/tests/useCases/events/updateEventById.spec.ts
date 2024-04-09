565c785588ee45b42ef96025dfd4f29dad0bf63c
Henrique Reis <contatohenriquereis@gmail.com>
2024-04-09 10:12:22 -0300

    feature: unit test that throws an exception when the ID is not provided to search for an event

--- src/tests/useCases/events/updateEventById.spec.ts
+++ src/tests/useCases/events/updateEventById.spec.ts
@@ -1,3 +1,4 @@
+import { CustomError } from '../../../domain/errors/customError'
 import { IEventRepository } from '../../../domain/interfaces/IEventRepository'
 import { UpdateEventByIdUseCase } from '../../../domain/useCases/events/updateEventById.useCase'
 import { EventRepositoryInMemory } from '../../mock/eventRepositoryInMemory'
@@ -48,5 +49,35 @@ describe('Update event by id', () => {
     expect(event.eventDescription).toBe('Mock Event - Updated')
 
     expect(mockEventRepository.findEventByIdAndUpdate).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should return an exception when the user ID is not provided for search and update', async () => {
+    const mockEvent = {
+      _id: '6602d505cfa1f38f545a4542',
+      eventName: 'Mock Event',
+      eventDescription: 'Mock Event',
+      cnpj: '12345678901220',
+      location: 'São Paulo',
+      eventType: 'Social Event',
+      eventTicketPrice: 1000,
+      venueCapacity: 1000,
+      contactInformation: 'Test mock event',
+      paymentMethod: 'Credit',
+    }
+
+    const { sut, mockEventRepository } = makeSut([mockEvent])
+
+    const newEventData = {
+      eventName: 'Mock Event - Updated',
+      eventDescription: 'Mock Event - Updated',
+    }
+
+    await expect(sut.execute(undefined, newEventData)).rejects.toThrow(
+      CustomError.BadRequestError(
+        'Event ID is required to proceed with update execution',
+      ),
+    )
+
+    expect(mockEventRepository.findEventByIdAndUpdate).not.toHaveBeenCalled()
   })
 })
