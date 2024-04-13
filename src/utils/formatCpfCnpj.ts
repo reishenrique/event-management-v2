110f94881f42da91d23c94bfb6d6efa17038dcde
Henrique Reis <contatohenriquereis@gmail.com>
2024-04-13 17:30:30 -0300

    refactor: separating the formatters into two functions

--- src/utils/formatCpfCnpj.ts
+++ src/utils/formatCpfCnpj.ts
@@ -1,3 +1,7 @@
-export function formatCpfCnpj(cpfCnpj: string): string {
-  return cpfCnpj.replace(/[.-]/g, '')
+export function formatCpf(cpf: string): string {
+  return cpf.replace(/[.-]/g, '')
+}
+
+export function formatCnpj(cnpj: string): string {
+  return cnpj.replace(/\D/g, '')
 }
