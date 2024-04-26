996b2fac80a01bea9a40b8cc63e7b987e902d78f
Henrique Reis <contatohenriquereis@gmail.com>
2024-04-26 15:36:24 -0300

    refactor: passing dependency injection of the user repository to the authentication controller

--- src/routes/authRoute/authRoutes.ts
+++ src/routes/authRoute/authRoutes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express'
 import AuthController from '../../controllers/authController'
 import verifyToken from '../../middlewares/authorization'
+import { UserRepository } from '../../application/repositories/userRepository'
+import { LoginUseCase } from '../../domain/useCases/auth/login.useCase'
 
 const authRoutes = Router()
 
-const authController = new AuthController()
+const userRepository = new UserRepository()
+
+const loginUseCase = new LoginUseCase(userRepository)
+
+const authController = new AuthController(loginUseCase)
 
 authRoutes.post('/auth/login', authController.login)
 authRoutes.post('/auth/verifyToken', verifyToken, authController.authenticated)
