91d2506b8b1e8f16da9b7f5ee4ec368e17a46631
Henrique Reis <contatohenriquereis@gmail.com>
2024-05-09 12:48:39 -0300

    refactor: removing token creation logic from the use case and implementing a specific function for user token creation

--- src/domain/useCases/auth/login.useCase.ts
+++ src/domain/useCases/auth/login.useCase.ts
@@ -1,22 +1,20 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
 import 'dotenv/config'
 import { CustomError } from '../../errors/customError'
 import { IUserRepository } from '../../interfaces/IUserRepository'
+import { generateAcessToken } from '../../../utils/jwtUtils'
 
 export class LoginUseCase {
   constructor(private userRepository: IUserRepository) {
     this.userRepository = userRepository
   }
 
-  async execute(loginUser: object): Promise<any> {
+  async execute(loginUser: object): Promise<string> {
     const {
       emailAddress,
       password,
     }: { emailAddress: string; password: string } = loginUser as any
-
-    const secret = process.env.SECRET
 
     const user = await this.userRepository.findUserByEmail(
       emailAddress as string,
@@ -35,13 +33,7 @@ export class LoginUseCase {
       throw CustomError.UnauthorizedError('Invalid password')
     }
 
-    const token = jwt.sign(
-      { id: user._id, email: user.emailAddress },
-      secret || '',
-      {
-        expiresIn: '8h',
-      },
-    )
+    const token = generateAcessToken(user)
 
     return token
   }
