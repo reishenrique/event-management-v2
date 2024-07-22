e6c9538d9a0956469fc361db48045aec8d2c337a
Henrique Reis <contatohenriquereis@gmail.com>
2024-07-22 16:08:08 -0300

    feat: setting api-docs route with swagger settings when the server intialize

--- src/infraestructure/http/app.ts
+++ src/infraestructure/http/app.ts
@@ -3,10 +3,15 @@ import eventRoutes from '../routes/eventRoute/eventRoutes'
 import userRoutes from '../routes/userRoute/userRoutes'
 import authRoutes from '../routes/authRoute/authRoutes'
 
+import swaggerUi from 'swagger-ui-express'
+import swaggerFile from '../../swagger-output.json'
+
 export const app: Express = express()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use('/api/v2', eventRoutes)
 app.use('/api/v2', userRoutes)
